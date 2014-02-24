4c0ce2b3e7d0b571e5de67cfa6bce921e89005d6
zagfai <zagfai@gmail.com>
2014-02-24 21:17:42 +0800

    maintain the dubug log output

--- webtul/db.py
+++ webtul/db.py
@@ -52,7 +52,8 @@ class MySQL:
         it will try x times to execute the sql, default is 1.
         """
         self.log and self.log.debug('SQL: ' + str(sql))
-        self.log and self.log.debug('PARAMs: ' + str(param))
+        if param is not ():
+            self.log and self.log.debug('PARAMs: ' + str(param))
         for i in xrange(times):
             try:
                 ret, res = self._execute(sql, param)
